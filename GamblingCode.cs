0da6e3ad7f92698431ed18c7b882ca59c2e47c31
Ingole987 <94819856+Ingole987@users.noreply.github.com>
2022-01-31 20:08:17 +0530

    added : Total Amount of Money

---
+++ GamblingCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingSimulations
+{
+    public class GamblingCode
+    {
+        int betWon, betLost;
+        int maxMoney = 100;
+        int moneySpend = 1;
+        Random random = new Random();
+       
+}
