124c729f0a50680f80f99f7ede5c26f77724b3ec
Ingole987 <pratik.ingole98@gmail.com>
2022-02-01 20:35:43 +0530

    added: win or loose

---
+++ GamblingCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingSimulations
+{
+    public class GamblingCode
+    {
+        int betWon, betLost;
+        int maxMoney = 100;
+        int moneySpend = 1;
+        public void GameWon()
+        {
+            if (betWon == 1)
+            {
+                Console.WriteLine("You have Won Bet");
+            }
+            else
+            {
+                Console.WriteLine("Play More");
+            }
+        }
+        public void GameLost()
+        {
+            if (betLost == 0)
+            {
+                Console.WriteLine("You have Lost Bet");
+            }
+            if (maxMoney == 0)
+            {
+                Console.WriteLine("Dont Have Enough Money");
+            }
+        }
+    }
+}
